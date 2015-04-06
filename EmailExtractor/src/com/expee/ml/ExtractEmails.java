ccb6401e42d0f2cd76ef4d24f3b4f04846b46f30
AnanyaKumar <skywalker94@gmail.com>
2015-04-05 23:34:33 -0400

    Refactor Extract Emails, Add tests

--- EmailExtractor/src/com/expee/ml/ExtractEmails.java
+++ EmailExtractor/src/com/expee/ml/ExtractEmails.java
@@ -1,10 +1,12 @@
 package com.expee.ml;
 
+import java.util.ArrayList;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.expee.ml.utils.Email;
 import com.expee.ml.utils.EmailParser;
@@ -14,55 +16,83 @@ public class ExtractEmails {
   private static final String BASE_DIR = "/Users/Ananya/Downloads/enron/maildir";
   private static final String OUTPUT = "EmailData.csv";
 
-  public static void addDirectedEdge(Map<Email, Email> emails, Email parent, Email child) {
+  private static void addDirectedEdge(Map<Email, Email> emails, Email parent, Email child) {
     emails.get(parent).addChild(emails.get(child));
   }
 
-  public static void updateChildren(Map<Email, Email> emails, ArrayList<Email> emailChain) {
+  private static void updateChildren(Map<Email, Email> emails, ArrayList<Email> emailChain) {
     for (int i = emailChain.size() - 1; i > 0; i--) {
       addDirectedEdge(emails, emailChain.get(i), emailChain.get(i - 1));
     }
   }
-
-  public static void extract(String dir, String output) throws IOException {
-    extract(dir, output, Integer.MAX_VALUE);
-  }
   
-  public static void extract(String dir, String output, int maxUsers) throws IOException {
-    File base = new File(dir);
-
-    // This might seem like a hack, but basically an email's contents is used as a hashcode
-    // for a specific instance of an email. We don't want references to different instances
-    // representing the same email to be floating around.
+  public static Set<Email> extractStructure(List<File> files) throws IOException {
     Map<Email, Email> emails = new HashMap<Email, Email>();
-    int usersSeen = 0;
+    
+    for (File emailFile : files) {
+     if (!emailFile.isFile()) continue;
+      
+      ArrayList<Email> emailChain = EmailParser.parseEmails(emailFile);
+      for (Email email : emailChain) {
+        if (!emails.containsKey(email)) {
+          emails.put(email, email);
+        }
+      }
+      updateChildren(emails, emailChain);
+    }
+
+    return emails.keySet();
+  }
+
+  public static List<File> getEmailFiles(String dir) throws IOException {
+    return getEmailFiles(dir, Integer.MAX_VALUE);
+  }
+
+  public static List<File> getLinearEmailFiles(String dir) {
+    File base = new File(dir);
+    List<File> emailFiles = new ArrayList<File>();
+
+    for (File emailFile : base.listFiles()) {
+      if (!emailFile.isFile() || emailFile.isHidden()) continue;
+      emailFiles.add(emailFile);
+    }
+
+    return emailFiles;
+  }
+
+  // This code is buggy. It only goes down 3 levels, instead of recursing.
+  public static List<File> getEmailFiles(String dir, int maxUsers) throws IOException {
+    List<File> emailFiles = new ArrayList<File>();
+    File base = new File(dir);
+    int numUsers = 0;
 
     for (File user : base.listFiles()) {
       if (!user.isDirectory()) continue;
       
       for (File folder : user.listFiles()) {
         if (!folder.isDirectory()) continue;
+        if (!folder.getName().equals("inbox")) continue;
         
         for (File emailFile : folder.listFiles()) {
-          if (!emailFile.isFile()) continue;
+          if (!emailFile.isFile() || emailFile.isHidden()) continue;
 
-          ArrayList<Email> emailChain = EmailParser.parseEmails(emailFile);
-          for (Email email : emailChain) {
-            if (!emails.containsKey(email)) {
-              emails.put(email, email);
-            }
-          }
-          updateChildren(emails, emailChain);
+          emailFiles.add(emailFile);
         }
       }
-      System.out.println(user.getName() + " " + emails.size());
-      if (++usersSeen >= maxUsers) break;
+
+      System.out.println(user.getName() + " " + emailFiles.size());
+      if (++numUsers >= maxUsers) break;
     }
-    System.out.println("Done?");
-    GetFeature.makeEmailSetFeatures(emails.keySet(), OUTPUT);
+
+    return emailFiles;
   }
-  
+
   public static void main(String[] args) throws Exception {
-    ExtractEmails.extract(BASE_DIR, OUTPUT, 10);
+    System.out.println("Getting email list");
+    List<File> emailFiles = getEmailFiles(BASE_DIR, 10);
+    System.out.println("Extracting email structure");
+    Set<Email> emails = extractStructure(emailFiles);
+    System.out.println("Making features");
+    GetFeature.makeEmailSetFeatures(emails, OUTPUT);
   }
 }
--- README.md
+++ README.md
@@ -3,4 +3,18 @@ Machine learning classifiers for email response prediction and generation on the
 
 10601 Machine Learning Project
 
-Peijin Zhang, Ananya Kumar, Li Chen Koh, Bojian Han
+Ananya Kumar, Bojian Han, Li Chen Koh, Peijin Zhang
+
+Compile: 
+Go to src, and run "javac com/expee/ml/ExtractEmails.java"
+
+Run:
+Go to src, and run "java com/expee/ml/ExtractEmails"
+
+Test:
+Go to src and run (without the quotes)
+"javac com/expee/ml/TestExtract.java"
+"java com/expee/ml/TestExtract"
+
+After making changes, make sure all tests pass.
+
